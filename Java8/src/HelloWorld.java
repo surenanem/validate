1d3268125ee6926a1be02a7a3e355126426c617f
sanem <sanem@L140070.stjude.sjcrh.local>
2017-11-04 21:34:05 -0500

    Initial commit from suren

---
+++ Java8/src/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author sanem
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("line1");
+
+	}
+
+}
